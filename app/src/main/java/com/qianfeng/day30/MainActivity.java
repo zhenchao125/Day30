eb8e79f28cccbe8f4cf03d2c7ef4932ff6df3090
lzc <30752740@qq.com>
2016-06-13 16:52:46 +0800

    第一次提交

---
+++ app/src/main/java/com/qianfeng/day30/MainActivity.java
@@ -0,0 +1,25 @@
+package com.qianfeng.day30;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*
+        在160dpi的屏幕上    手机的宽度1个in
+        160px的线 1in
+
+        1dp=1px
+        160dp的线  1in
+        在320dpi的屏幕上    手机的宽度是1个in
+        160px的线 0.5in
+
+        1dp=2px
+        160dp的线  1in
+         */
+    }
+}
